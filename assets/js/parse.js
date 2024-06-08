364f1871d93e108ac4776bf635c5348851a131b0
Onu <onucsecu@gmail.com>
2024-06-08 22:18:54 +0900

    content load from yaml

--- assets/js/parse.js
+++ assets/js/parse.js
@@ -1,12 +1,14 @@
-fetch("")
+fetch("https://raw.githubusercontent.com/onucsecu2/onucsecu2.github.io/remaster/assets/data/information.yaml")
     .then(response => response.text())
     .then(data => {
-        // const yamlContentElement = document.getElementById("yaml-content");
-        // yamlContentElement.textContent = data;
-        console.log(data)
+        const yaml = jsyaml.load(data);
+        yaml["SECTIONS"].forEach((section) => {
+
+        })
     })
     .catch(error => {
         console.error("Error loading YAML file:", error);
         // const yamlContentElement = document.getElementById("yaml-content");
         // yamlContentElement.textContent = "Error loading YAML file";
     });
+
