b8f4775618f0e786810cc19098fb838fe41f9c92
Onu <onucsecu@gmail.com>
2024-06-12 22:18:16 +0900

    vanilla js and css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,23 @@
+// (function() {
+//     let sectionElems = document.getElementsByTagName('section');
+//     for(let i=0; i<sectionElems.length; i++) {
+//         if (i%2===1){
+//             sectionElems[i].classList.add('section-bg');
+//         }
+//     }
+// })();
 (function() {
-    let sectionElems = document.getElementsByTagName('section');
-    for(let i=0; i<sectionElems.length; i++) {
-        if (i%2===1){
-            sectionElems[i].classList.add('section-bg');
-        }
-    }
+    let locationImageElem = document.getElementById('callout-location');
+    let locationTextElem = document.getElementById('location');
+    locationTextElem.addEventListener("mouseenter", function (e) {
+        locationImageElem.style.display="inline-block";
+        locationImageElem.classList.add("animate-fade");
+        locationImageElem.classList.remove("fade-out");
+
+
+    })
+
+    locationTextElem.addEventListener("mouseout", function (e) {
+        locationImageElem.classList.add("fade-out");
+    })
 })();
